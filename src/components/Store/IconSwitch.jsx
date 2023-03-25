f60b17c04e0e7e9b74e631b93c39821df3d6d83a
IlyaAlpatov <alpatov30@gmail.com>
2023-03-25 18:56:48 +0300

    добавлено переключение иконки

--- src/components/Store/IconSwitch.jsx
+++ src/components/Store/IconSwitch.jsx
@@ -1,2 +1,16 @@
-const IconSwitch = () => {};
+import "material-icons/iconfont/material-icons.css";
+import IconButton from "@mui/material/IconButton";
+
+const IconSwitch = ({ icon, onSwitch }) => {
+  const handleClick = () => {
+    onSwitch();
+  };
+  return (
+    <div className="icon-container">
+      <IconButton onClick={handleClick}>
+        <span className="material-icons view-icon">{icon}</span>
+      </IconButton>
+    </div>
+  );
+};
 export default IconSwitch;
--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -1,3 +1,22 @@
-const Store = () => {};
+import storeProducts from "../../store/index";
+import { useState } from "react";
+import IconSwitch from "./IconSwitch";
+
+const Store = () => {
+  const products = storeProducts;
+
+  const [icon, setIcon] = useState("view_module");
+
+  const onSwitch = () => {
+    const newIcon = icon === "view_module" ? "view_list" : "view_module";
+    setIcon(newIcon);
+  };
+
+  return (
+    <div className="store">
+      <IconSwitch icon={icon} onSwitch={onSwitch} />
+    </div>
+  );
+};
 
 export default Store;
