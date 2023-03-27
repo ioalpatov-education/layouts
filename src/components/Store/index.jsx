bc809dd92e269da024066242f56c0b56c7a864d5
IOAlpatov <IOAlpatov@kaluga.beeline.ru>
2023-03-27 10:26:52 +0300

    в компонент стора добавлен вывод компонентов view в зависимости от иконки

--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -1,20 +1,30 @@
-import storeProducts from "../../store/index";
+import productsFromStore from "../../store/index";
 import { useState } from "react";
 import IconSwitch from "./IconSwitch";
+import CardsView from "./CardsView";
+import ListView from "./ListView";
 
 const Store = () => {
-  const products = storeProducts;
+  const products = productsFromStore;
 
-  const [icon, setIcon] = useState("view_module");
+  const [icon, setIcon] = useState("view_list");
 
   const onSwitch = () => {
     const newIcon = icon === "view_module" ? "view_list" : "view_module";
     setIcon(newIcon);
   };
 
+  const view =
+    icon === "view_module" ? (
+      <ListView items={products} />
+    ) : (
+      <CardsView cards={products} />
+    );
+
   return (
     <div className="store">
       <IconSwitch icon={icon} onSwitch={onSwitch} />
+      {view}
     </div>
   );
 };
